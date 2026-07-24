21ffdfe20864e0f5d5876b47eb3e39e91b1e9cb7
colinb <colinb@accesspointmg.com>
2026-07-23 22:07:54 -0700

    remove stray debug script

--- _check_resolve.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0, r"F:\github\accesspointmg\o3de-cli")
-from o3de_cli.core.resolver import Resolver
-
-r = Resolver()
-r.resolve()
-gem = r.objects.get("org.o3de.gem.achievementstest")
-print("gem:", gem.name, gem.version, gem.path)
-print("matched overlays:", [(o.name, o.data.get("extends")) for o in gem.overlays])
